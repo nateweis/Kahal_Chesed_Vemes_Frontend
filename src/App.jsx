bb63c55545b170e0d2143c708f73eb9274f032fa
Naftali Weiss <Naftali@PROPUMP.local>
2020-11-23 13:58:37 -0500

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
 class App extends Component{
 
   render(){
     return(
-      <>
-        <h1>Shule React App</h1>
-      </>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route path="/" exact render={()=> <Redirect to="/wine-store/home" />} />
+          <Route path="/wine-store/home" exact render={({history})=><HomePage push={history.push} />} /> */}
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
