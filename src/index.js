9b99a1f2e66b6d22cbdb36282a20a4886b6a4604
Naftali Weiss <Naftali@PROPUMP.local>
2020-11-23 14:55:49 -0500

    added redux store to react app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+////////// STORE -> GLOBALIZED STATE
+import {createStore, applyMiddleware, compose} from 'redux'
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const initState = {};
+const middleware = [thunk]
+
+const store = createStore(
+  allReducers,
+  initState,
+  compose(
+          applyMiddleware(...middleware),
+          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+  );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
