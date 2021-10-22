dd8fdc02460c34bee96a074e3fc41466d0dfcc81
Naftali Weiss <Naftali@PROPUMP.local>
2021-10-22 12:13:51 -0400

    animated the flash message

--- src/components/Template/FlashMessage.jsx
+++ src/components/Template/FlashMessage.jsx
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 
 class FlashMessage extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            flashMessage : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis quaerat ullam tenetur porro alias illo eligendi, accusamus quo quasi? Nihil laudantium debitis, pariatur voluptates nemo eaque? In cum enim suscipit?'
+        }
+    }
 
     render(){
         return(
             <>  
                 <div style={style.messageContainer}>
-                    <div style={style.message}>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis quaerat ullam tenetur porro alias illo eligendi, 
-                        accusamus quo quasi? Nihil laudantium debitis, pariatur voluptates nemo eaque? In cum enim suscipit?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, numquam pariatur nostrum aliquid, consequatur magnam quisquam architecto laudantium aut ex impedit 
-                        culpa temporibus mollitia fuga nisi error amet necessitatibus quasi?
+                    <div className="animateMessage" style={style.message}>
+                        {this.state.flashMessage}
+                        
                     </div>
                 </div>
             </>
@@ -20,7 +24,7 @@ class FlashMessage extends Component{
 
 const style = {
     message:{
-        width : 'max-content',
+        whiteSpace: 'nowrap',
         fontSize : '18px'
     },
     messageContainer :{
