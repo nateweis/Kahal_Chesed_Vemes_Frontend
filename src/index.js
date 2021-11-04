664ea801a6f1c7d2f6acc5717a972f6aa0485e5e
Naftali Weiss <Naftali@PROPUMP.local>
2021-11-04 11:56:20 -0400

    removed redux to see if that will help

--- src/index.js
+++ src/index.js
@@ -3,29 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-////////// STORE -> GLOBALIZED STATE
-import {createStore, applyMiddleware, compose} from 'redux'
-import allReducers from './reducers';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-
-const initState = {};
-const middleware = [thunk]
-
-const store = createStore(
-  allReducers,
-  initState,
-  compose(
-          applyMiddleware(...middleware),
-          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
-  );
-
-ReactDOM.render(
-  <Provider store = {store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
