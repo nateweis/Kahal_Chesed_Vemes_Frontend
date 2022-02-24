53b39db49e8a7633ab1cf4dc13dba5bf76ed4e1f
Naftali Weiss <Naftali@PROPUMP.local>
2022-02-24 11:10:04 -0500

    set up the scroll across top feature of flash message and even improved the cut in/out timing

--- src/components/FlashMessage.jsx
+++ src/components/FlashMessage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import styled from "styled-components";
+import React, {useRef, useState, useEffect} from 'react'
+import styled, { keyframes } from "styled-components";
 
 const FlashMessage = () => {
+  const [width, setWidth] = useState(30)
+  const refMessage = useRef(null)
+  useEffect(() => {
+    setWidth(refMessage.current.offsetWidth)
+  })
+
   return (
     <Container>
-        Flash Message Scrolling By 
+        <Wrapper ref={refMessage} num={width} time={20} >
+            Lorem ipsum dolor sit expedita sed voluptatibus deleniti officia tempora corrupti iste nihil, pariatur veniam?
+        </Wrapper> 
     </Container>
   )
 }
@@ -14,9 +22,23 @@ const Container = styled.div`
     height: 45px;
     background-color: #514747;
     color: var(--white);
+    font-size: 20px;
+    overflow: 'hidden';
     display: flex;
     align-items: center;
-    font-size: 20px;
 `
+
+const animateWords = (width) => keyframes`
+  0%{transform: translateX(100vw);}
+  100%{transform: translateX(-${width}px);}
+`
+
+const Wrapper = styled.div` 
+  min-width: max-content;
+  height: 75% ;
+  animation: ${props => animateWords(props.num)} ${props => props.time}s linear infinite;
+
+`
+
 
 export default FlashMessage
