c5d8183ba76d703c19ebe4fd045cfc18413b7890
Naftali Weiss <Naftali@PROPUMP.local>
2022-03-01 16:19:20 -0500

    now shares the data from zman to the info module

--- src/reducers/OverlayReducer.js
+++ src/reducers/OverlayReducer.js
@@ -2,13 +2,14 @@ import {createSlice} from '@reduxjs/toolkit';
 
 export const OverlayReducer = createSlice({
     name: 'overlay',
-    initialState: {active:false, slide: false},
+    initialState: {active:false, slide: false, title: "Title", data: {}},
     reducers:{
         overlayActive: (state, action)=> {state.active = action.payload; state.slide = action.payload},
         slideOut: (state) => {state.slide = false},
+        updateData: (state, action) => {state.title = action.payload.title; state.data = action.payload.data},
     }
 })
 
 export default OverlayReducer.reducer
 
-export const {overlayActive, slideOut} = OverlayReducer.actions
+export const {overlayActive, slideOut, updateData} = OverlayReducer.actions
